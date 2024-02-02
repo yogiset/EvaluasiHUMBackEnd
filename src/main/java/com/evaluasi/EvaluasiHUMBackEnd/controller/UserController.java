eea50317c5cac89a9a708daf5f4817ed860137f0
yogiset <the3verlastin9@gmail.com>
2024-02-03 01:50:53 +0700

    added controller for login,update,and delete

--- src/main/java/com/evaluasi/EvaluasiHUMBackEnd/controller/UserController.java
+++ src/main/java/com/evaluasi/EvaluasiHUMBackEnd/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.evaluasi.EvaluasiHUMBackEnd.controller;
 
+import com.evaluasi.EvaluasiHUMBackEnd.dto.AuthResponse;
+import com.evaluasi.EvaluasiHUMBackEnd.dto.RuleDto;
 import com.evaluasi.EvaluasiHUMBackEnd.dto.UserDto;
 import com.evaluasi.EvaluasiHUMBackEnd.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -28,4 +30,36 @@ public class UserController {
     public List<UserDto> showAlluser(){
         return userService.showall();
     }
+
+
+    @PutMapping(path = "/edituser/{id}")
+    public ResponseEntity<Object>editUser(@PathVariable("id")Long id,@RequestBody UserDto userDto) {
+        try {
+            return userService.editUser(id,userDto);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping(path = "/hapususer/{id}")
+    public ResponseEntity<Object>hapusUser(@PathVariable("id")Long id) {
+        try {
+            return userService.hapusUser(id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<Object>login(@RequestBody AuthResponse authResponse){
+        try {
+            return userService.login(authResponse);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
