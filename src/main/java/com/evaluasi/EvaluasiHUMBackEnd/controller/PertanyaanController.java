8ed2248f6f6b3132e442089a1b5cae4bf35d9ec1
yogiset <the3verlastin9@gmail.com>
2024-02-02 12:07:15 +0700

    added Get and add service,controller

--- src/main/java/com/evaluasi/EvaluasiHUMBackEnd/controller/PertanyaanController.java
+++ src/main/java/com/evaluasi/EvaluasiHUMBackEnd/controller/PertanyaanController.java
@@ -1,9 +1,13 @@
 package com.evaluasi.EvaluasiHUMBackEnd.controller;
 
+import com.evaluasi.EvaluasiHUMBackEnd.dto.PertanyaanDto;
 import com.evaluasi.EvaluasiHUMBackEnd.service.PertanyaanService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PertanyaanController {
     private final PertanyaanService pertanyaanService;
 
+    @PostMapping(path = "/addpertanyaan")
+    public ResponseEntity<Object> addPertanyaan(@RequestBody PertanyaanDto pertanyaanDto) {
+        try {
+            return pertanyaanService.addPertanyaan(pertanyaanDto);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/all")
+    public List<PertanyaanDto> showAllPertanyaan(){
+        return pertanyaanService.showAll();
+    }
 }
--- src/main/java/com/evaluasi/EvaluasiHUMBackEnd/service/PertanyaanService.java
+++ src/main/java/com/evaluasi/EvaluasiHUMBackEnd/service/PertanyaanService.java
@@ -1,12 +1,67 @@
 package com.evaluasi.EvaluasiHUMBackEnd.service;
 
+import com.evaluasi.EvaluasiHUMBackEnd.dto.PertanyaanDto;
+import com.evaluasi.EvaluasiHUMBackEnd.entity.Karyawan;
+import com.evaluasi.EvaluasiHUMBackEnd.entity.Pertanyaan;
+import com.evaluasi.EvaluasiHUMBackEnd.repository.KaryawanRepository;
 import com.evaluasi.EvaluasiHUMBackEnd.repository.PertanyaanRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PertanyaanService {
     private final PertanyaanRepository pertanyaanRepository;
+    private final KaryawanRepository karyawanRepository;
 
+    public ResponseEntity<Object> addPertanyaan(PertanyaanDto pertanyaanDto) {
+        try{
+            log.info("Inside Add Pertanyaan");
+            Pertanyaan pertanyaan = new Pertanyaan();
+            pertanyaan.setKodepertanyaan(pertanyaanDto.getKodepertanyaan());
+            pertanyaan.setPertanyaan(pertanyaanDto.getPertanyaan());
+            pertanyaan.setJawaban(pertanyaanDto.getJawaban());
+            pertanyaan.setBobot(pertanyaanDto.getBobot());
+
+
+            Karyawan karyawan = karyawanRepository.findByNik(pertanyaanDto.getNik());
+            if (karyawan == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Karyawan not found for NIK: " + pertanyaanDto.getNik());
+            }
+
+            pertanyaan.setKaryawan(karyawan);
+            pertanyaanRepository.save(pertanyaan);
+
+            return ResponseEntity.ok("Pertanyaan created successfully");
+        } catch (Exception e) {
+            log.error("Error creating pertanyaan", e);
+            return ResponseEntity.status(500).body("Error creating pertanyaan");
+        }
+
+    }
+
+    public List<PertanyaanDto> showAll() {
+        log.info("Inside showall");
+        List<Pertanyaan>pertanyaanList = pertanyaanRepository.findAll();
+
+        return pertanyaanList.stream()
+                .map(tanya -> {
+                    PertanyaanDto pertanyaanDto = new PertanyaanDto();
+                    pertanyaanDto.setIdper(tanya.getIdper());
+                    pertanyaanDto.setKodepertanyaan(tanya.getKodepertanyaan());
+                    pertanyaanDto.setPertanyaan(tanya.getPertanyaan());
+                    pertanyaanDto.setJawaban(tanya.getJawaban());
+                    pertanyaanDto.setBobot(tanya.getBobot());
+                    pertanyaanDto.setNik(tanya.getKaryawan().getNik());
+                    return pertanyaanDto;
+                })
+                .collect(Collectors.toList());
+    }
 }
