8b80c20ce7424ac67e96dea46194cfe2aca58922
Yoghi Setiawan <137569101+yogiset@users.noreply.github.com>
2024-02-03 21:21:10 +0700

    Delete src/main/java/com/evaluasi/EvaluasiHUMBackEnd/constant directory

--- src/main/java/com/evaluasi/EvaluasiHUMBackEnd/constant/ApiConstant.java
+++
@@ -1,7 +0,0 @@
-package com.evaluasi.EvaluasiHUMBackEnd.constant;
-
-public class ApiConstant {
-    public static final String SOMETHING_WENT_WRONG="Something Went Wrong";
-
-    public static final String INVALID_DATA="Invalid Data.";
-}
