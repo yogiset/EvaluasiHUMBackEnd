0331746d470dd11794ee01b178bc307dd5afa9dc
yogiset <the3verlastin9@gmail.com>
2024-04-02 09:52:47 +0700

    add entity Cpt

---
+++ src/main/java/com/evaluasi/EvaluasiHUMBackEnd/entity/Cpt.java
@@ -0,0 +1,30 @@
+package com.evaluasi.EvaluasiHUMBackEnd.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Cpt {
+    @Id
+    @SequenceGenerator(name = "cpt_sequence",sequenceName = "cpt_sequence",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "cpt_sequence")
+    private Long idcpt;
+    private int tahun;
+    private int panolcustomer;
+    private int coverage;
+    private int coveragepersen;
+    private int penetration;
+    private double throughput;
+    private int hitrate;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "nik", nullable = false)
+    private Karyawan karyawan;
+}
