2a50fb73dfb300f498f94f4b5757defcbb80facc
yogiset <the3verlastin9@gmail.com>
2024-04-02 12:00:55 +0700

    add CptDto

---
+++ src/main/java/com/evaluasi/EvaluasiHUMBackEnd/dto/CptDto.java
@@ -0,0 +1,21 @@
+package com.evaluasi.EvaluasiHUMBackEnd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CptDto {
+    private Long idcpt;
+    private String nik;
+    private String nama;
+    private int tahun;
+    private int panolcustomer;
+    private int coverage;
+    private double coveragepersen;
+    private int penetration;
+    private String throughput;
+    private double hitrate;
+}
